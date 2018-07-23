2996c8d84b2da2e5b8704937f4569e91ff2477b6
Samir Zenag <samzenag@gmail.com>
2018-07-23 18:45:18 +0200

    playing with sinatra

--- app.rb
+++ app.rb
@@ -2,11 +2,25 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+
+
+# csv_file   = File.join(__dir__, 'recipes.csv')
+# cookbook   = Cookbook.new(csv_file)
+# controller = Controller.new(cookbook)
+
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  'Le wagon'
+  erb :index
+end
+get '/about' do
+  erb :about
+end
+get '/team/:username' do
+   puts params[:username]
+  "The username is #{params[:username]}"
 end
